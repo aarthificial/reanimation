62e530f541e9898d340f91f51660b0e44116304a
aarthificial <aarthificial@gmail.com>
2021-01-20 18:30:09 +0100

    docs: Add README, LICENSE, and documentation for the renderer

---
+++ README.md
@@ -0,0 +1,35 @@
+# Reanimator
+
+Reanimator is a custom animator for Unity created 
+[to simplify the development of Astortion](https://youtu.be/nA2IChvy_QU).
+
+It's tailored specifically for traditional animations - animations represented by a series of 
+cels displayed one after the other.
+<br>
+It's not modeled using a **finite state machine** but rather a **tree-like graph** - mitigating 
+the "transition hell" problem while still providing all the basic functionalities you would expect 
+an animation system to provide:
+
+- animation switching
+- transitional/one-off animations
+- percentage-driven animations
+- animation overrides
+- events
+
+### Installation
+Reanimator can be [installed as a unity package](https://docs.unity3d.com/Manual/upm-ui-giturl.html) 
+using this url:
+```
+https://github.com/aarthificial/reanimation
+```
+I'm still not sure if and when it will land on the Asset Store.
+
+### Release video:
+[![Saying goodbye to Animation State Machines](https://img.youtube.com/vi/5aHhmRiVpZI/maxresdefault.jpg)](https://youtu.be/5aHhmRiVpZI)
+
+Please note, however, that I'm not claiming that this solution is better than the built-in animator. 
+<br>
+*It's an interesting alternative, that's it.*
+<br>
+I'm releasing it due to a lot of requests I got under my devlog, 
+not because I'm trying to popularize it or something.
--- Runtime/Reanimator.cs
+++ Runtime/Reanimator.cs
@@ -9,20 +9,58 @@ namespace Aarthificial.Reanimation
 
     public class Reanimator : MonoBehaviour
     {
+        /// <summary>
+        /// Event triggered each time the graph is resolved.
+        /// 
+        /// Frequency depends on the <see cref="Reanimator.fps">framerate</see>.
+        /// </summary>
         public event ReanimatorListener Ticked;
+
+        /// <summary>
+        /// SpriteRenderer being used to display the animation.
+        /// </summary>
         public SpriteRenderer Renderer => renderer;
-        public ReanimatorNode root;
+
+        /// <summary>
+        /// State used during the previous resolution.
+        /// 
+        /// Should not be modified.
+        /// </summary>
         public IReadOnlyReanimatorState State => _previousState;
+
+        /// <summary>
+        /// State that will be used during the next resolution.
+        /// 
+        /// Should not be modified directly.<br/>
+        /// Use the <see cref="Reanimator.Set(string, int)">Set() method</see> instead
+        /// </summary>
         public IReadOnlyReanimatorState NextState => _nextState;
 
+        /// <summary>
+        /// Shortcut for setting the <see cref="ReanimatorState.FlipDriver"/>
+        /// </summary>
         public bool Flip
         {
             set => _previousState.Set(ReanimatorState.FlipDriver, value);
         }
 
-        [SerializeField] private new SpriteRenderer renderer;
-        [SerializeField] private int fps = 10;
-        [SerializeField] private string[] temporaryDrivers = new string[0];
+        /// <summary>
+        /// Root of the graph being used to resolve cels.
+        /// </summary>
+        [Tooltip("Root of the graph being used to resolve cels.")]
+        public ReanimatorNode root;
+
+        [Tooltip("SpriteRenderer being used to display the animation.")] 
+        [SerializeField]
+        private new SpriteRenderer renderer;
+
+        [Tooltip("Framerate of the displayed animation.")] 
+        [SerializeField]
+        private int fps = 12;
+
+        [Tooltip("Drivers marked as temporary are removed from the state if they were not set during the previous resolution.")]
+        [SerializeField]
+        private string[] temporaryDrivers = new string[0];
 
         private readonly Dictionary<string, ReanimatorListener> _listeners =
             new Dictionary<string, ReanimatorListener>();
@@ -92,26 +130,63 @@ namespace Aarthificial.Reanimation
             Ticked?.Invoke();
         }
 
+        /// <summary>
+        /// Modify driver in the next state.
+        /// </summary>
+        /// <param name="key">Name of the driver</param>
+        /// <param name="value">New value</param>
         public void Set(string key, int value)
         {
             _nextState.Set(key, value);
         }
 
+        /// <summary>
+        /// Modify driver in the next state.
+        ///
+        /// Used in conjunction with <see cref="ControlDriver.percentageBased"/>
+        /// to control the completion of animation using a percentage-based value:
+        /// <code>
+        /// 0.0f = 0%
+        /// 0.5f = 50%
+        /// 1.0f = 100%
+        /// </code>
+        /// </summary>
+        /// <param name="key">Name of the driver</param>
+        /// <param name="value">New value</param>
         public void Set(string key, float value)
         {
             _nextState.Set(key, value);
         }
 
+        /// <summary>
+        /// Modify driver in the next state.
+        ///
+        /// Shortcut for:
+        /// <code>value ? 1 : 0</code>
+        /// </summary>
+        /// <param name="key">Name of the driver</param>
+        /// <param name="value">New value</param>
         public void Set(string key, bool value)
         {
             _nextState.Set(key, value);
         }
 
+        /// <summary>
+        /// Return true if the value of the driver will change during the next resolution. 
+        /// </summary>
+        /// <param name="key">Name of the driver</param>
+        /// <returns></returns>
         public bool WillChange(string key)
         {
             return _nextState.Contains(key) && _nextState.Get(key) != _previousState.Get(key);
         }
 
+        /// <summary>
+        /// Return true if the value of the driver will change to the specified value during the next resolution. 
+        /// </summary>
+        /// <param name="key">Name of the driver</param>
+        /// <param name="toValue">Expected new value</param>
+        /// <returns></returns>
         public bool WillChange(string key, int toValue)
         {
             if (!_nextState.Contains(key)) return false;
@@ -120,6 +195,15 @@ namespace Aarthificial.Reanimation
             return nextValue == toValue && _previousState.Get(key) != nextValue;
         }
 
+        /// <summary>
+        /// Return true if the value of the driver will change to the specified value during the next resolution.
+        ///
+        /// Shortcut for:
+        /// <code>toValue ? 1 : 0</code>
+        /// </summary>
+        /// <param name="key">Name of the driver</param>
+        /// <param name="toValue">Expected new value</param>
+        /// <returns></returns>
         public bool WillChange(string key, bool toValue)
         {
             if (!_nextState.Contains(key)) return false;
@@ -128,12 +212,23 @@ namespace Aarthificial.Reanimation
             return nextValue == toValue && _previousState.GetBool(key) != nextValue;
         }
 
+        /// <summary>
+        /// Immediately force the next resolution process to happen.
+        /// </summary>
         public void ForceRerender()
         {
             _clock = 0;
             UpdateFrame();
         }
 
+        /// <summary>
+        /// Add listener for the given driver.
+        ///
+        /// Listeners are invoked whenever the driver is set during a resolution process.<br/>
+        /// The value does not matter. 
+        /// </summary>
+        /// <param name="driverName"></param>
+        /// <param name="listener"></param>
         public void AddListener(string driverName, ReanimatorListener listener)
         {
             if (_listeners.ContainsKey(driverName))
@@ -142,6 +237,11 @@ namespace Aarthificial.Reanimation
                 _listeners[driverName] = listener;
         }
 
+        /// <summary>
+        /// Remove listener for the given driver.
+        /// </summary>
+        /// <param name="driverName"></param>
+        /// <param name="listener"></param>
         public void RemoveListener(string driverName, ReanimatorListener listener)
         {
             if (!_listeners.ContainsKey(driverName)) return;
@@ -151,11 +251,19 @@ namespace Aarthificial.Reanimation
                 _listeners.Remove(driverName);
         }
 
+        /// <summary>
+        /// Define additional temporary drivers.
+        /// </summary>
+        /// <param name="driverName"></param>
         public void AddTemporaryDriver(params string[] driverName)
         {
             _temporaryDrivers.UnionWith(driverName);
         }
 
+        /// <summary>
+        /// Remove temporary drivers.
+        /// </summary>
+        /// <param name="driverName"></param>
         public void RemoveTemporaryDriver(params string[] driverName)
         {
             _temporaryDrivers.ExceptWith(driverName);
